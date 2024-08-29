a0cd2f15c1bbf4c15be89f8fbc9bea5eca09ffb7
Deo-Ekodi <deoekodi@gmail.com>
2024-08-29 08:10:10 -0800

    wastewater_model header + generic output_handler

--- include/core/waste_water_model.hpp
+++ include/core/waste_water_model.hpp
@@ -1,4 +1,20 @@
-#pragma once
+#ifndef WWM_H
+#define WWM_H
+
+#include <vector>
+#include <memory>
+#include <string>
+#include <stdexcept>
+#include <iostream>
+
+#include "../io/output_handler.hpp"
+#include "../io/config_parser.hpp"
+#include "../io/input_handler.hpp"
+#include "boundary_conditions.hpp"
+#include "output_handler.hpp"
+#include "results.hpp"
+#include "pipe.hpp"
+
 
 /**
  * WasteWaterModel (alias WWM)
@@ -20,13 +36,54 @@
 
 namespace core
 {
+    class WasteWaterModelException : public std::runtime_error
+    {
+    public:
+        explicit WasteWaterModelException(const std::string& msg)
+            : std::runtime_error (msg) {}
+    };
+    
     class WasteWaterModel
     {
     private:
-        /* data */
+        /* set up pipe network */
+        void setup_pipe__network(const InputHandler&);
+        /* validate model setup */
+        void validate_model() const;
+        /* solve flow equations */
+        void solve_flow_equations();
+        /* update results after simulation */
+        void update_results();
+
+        std::vector<std::shared_ptr<Pipe> > pipes;
+        Results simulation_results;
+        std::shared_ptr<BoundaryConditions> boundary_conditions;
+        double simulation_time;
+        double timestep;
     public:
-        WasteWaterModel(/* args */);
-        ~WasteWaterModel();
+        WasteWaterModel(/* args */) = default;
+
+        WasteWaterModel (const WasteWaterModel&) = delete;
+        WasteWaterModel& operator= (const WasteWaterModel&) = delete;
+
+        WasteWaterModel (WasteWaterModel&&) noexcept = default;
+        WasteWaterModel& operator= (WasteWaterModel&&) noexcept = default;
+
+        /* initialize model parameters */
+        void initialize (const InputHandler&, const ConfigParser&);
+        /* run simulation */
+        void run_simulation();
+        /* boundary conditions - if any */
+        void apply_boundary_conditions(const BoundaryConditions&);
+        /* retrieve results */
+        const Results& get_results() const;
+        /* real-time modification of results */
+        void modify_results(OutputHandler&);
+
+        ~WasteWaterModel() noexcept = default;
     };
 
 } // namespace core
+
+
+#endif
